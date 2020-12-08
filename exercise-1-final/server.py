e9976b2268a9b7cd53f5e41dd13518fca4c7130c
matildepeixoto <matilde.peixoto@tecnico.ulisboa.pt>
2020-12-08 21:40:01 +0000

    first commit

---
+++ exercise-1-final/server.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+from flask import Flask, abort, request,  redirect, url_for
+from Video_DB import *
+from time import sleep
+app = Flask(__name__)
+
+@app.route("/API/videos/", methods=['GET'])
+def returnsVideosJSON():
+    return {"videos": listVideosDICT()}
+
+
+@app.route("/API/videos/<int:id>/")
+def returnSingleVideoJSON(id):
+    try:
+        v = getVideoDICT(id)
+        return v
+    except:
+        abort(404)
+
+
+@app.route("/API/videos/", methods=['POST'])
+def createNewVideo():
+    sleep(0.1)
+    j = request.get_json()
+    print (type(j))
+    ret = False
+    try:
+        print(j["description"])
+        ret = newVideo(j["description"], j["url"])
+    except:
+        abort(400)
+        #the arguments were incorrect
+    if ret:
+        return {"id": ret}
+    else:
+        abort(409)
+    #if there is an erro return ERROR 409
+
+
+@app.route("/API/videos/<int:id>/views", methods=['PUT', 'PATCH'])
+def newView(id):
+    try:
+        return {"id":id, "views": newVideoView(id)}
+    except:
+        abort(404)
+
+
+
+
+@app.route("/")
+def index():
+    return app.send_static_file('index.html')
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ oauth-FENIX/app.py
@@ -0,0 +1,80 @@
+from flask import Flask
+from flask_dance.consumer import OAuth2ConsumerBlueprint
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import jsonify, url_for
+from flask import session
+
+
+import requests
+
+
+#necessary so that our server does not need https
+import os
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+# Click details do get the Client Id and Client Secret and fill the next constructor
+app = Flask(__name__)
+app.secret_key = "supersekrit"  # Replace this with your own secret!
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_PERMANENT'] = False
+fenix_blueprint = OAuth2ConsumerBlueprint(
+    "fenix-example", __name__,
+    # this value should be retrived from the FENIX OAuth page
+    client_id="1695915081466092",
+    # this value should be retrived from the FENIX OAuth page
+    client_secret="vmumAkyvslJjcTB7J0hoeqvrBqi9drd7JOsxBbkPtozV20XeGtHYeMFMISZnYz3IKW0oRxmFCE0LLSfLf0BVzQ==",
+    # do not change next lines
+    base_url="https://fenix.tecnico.ulisboa.pt/",
+    token_url="https://fenix.tecnico.ulisboa.pt/oauth/access_token",
+    authorization_url="https://fenix.tecnico.ulisboa.pt/oauth/userdialog",
+)
+
+app.register_blueprint(fenix_blueprint)
+
+
+
+@app.route('/')
+def home_page():
+    # The access token is generated everytime the user authenticates into FENIX
+    print(fenix_blueprint.session.authorized)
+    print("Access token: "+ str(fenix_blueprint.session.access_token))
+    return render_template("appPage.html", loggedIn = fenix_blueprint.session.authorized)
+
+
+
+@app.route('/logout')
+def logout():
+    # this clears all server information about the access token of this connection
+    res = str(session.items())
+    print(res)
+    session.clear()
+    res = str(session.items())
+    print(res)
+    # when the browser is redirected to home page it is not logged in anymore
+    return redirect(url_for("home_page"))
+
+    
+
+@app.route('/private')
+def private_page():
+    #this page can only be accessed by a authenticated user
+
+    # verification of the user is  logged in
+    if fenix_blueprint.session.authorized == False:
+        #if not logged in browser is redirected to login page (in this case FENIX handled the login)
+        return redirect(url_for("fenix-example.login"))
+    else:
+        #if the user is authenticated then a request to FENIX is made
+        resp = fenix_blueprint.session.get("/api/fenix/v1/person/")
+        #resp contains the response made to /api/fenix/vi/person (information about current user)
+        data = resp.json() 
+        print(resp.json())
+        return render_template("privPage.html", username=data['username'], name=data['name'])
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
